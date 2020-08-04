93a7b5a2a5c1fc30b8d4cf1299182cef3c8a60ef
Stuart Pineo <svpineo@gmail.com>
2020-08-04 08:58:09 -0400

    Started work on Author/Event driven quote.

--- myquotes/views.py
+++ myquotes/views.py
@@ -2,16 +2,34 @@ from django.http import HttpResponse
 
 from django.template import loader
 
-from myquotes.models import Quotation
+from myquotes.models import Quotation, Event
 
 import random
+import datetime
 
+# Return a random quotation associated with an author event for today
+# or just a random quotation if now event found
+#
 def index(request):
+    now = datetime.datetime.now()
 
-    count       = Quotation.objects.count()
-    rand_num    = random.randint(1, count)
+    year      = now.year
+    month     = now.month
+    day       = now.day
 
-    quotation   = Quotation.objects.all()[rand_num:rand_num+1]
+    # Query Event
+    #
+    events_count   = Event.objects.filter(month=month).count()
+    event_rand_num = random.randint(1, events_count)
+    
+    event          = Event.objects.filter(month=month)[event_rand_num:event_rand_num+1]
+    if event:
+        print(event[0])
+
+    all_count = Quotation.objects.count()
+    rand_num  = random.randint(1, all_count)
+
+    quotation = Quotation.objects.all()[rand_num:rand_num+1]
 
     template = loader.get_template('index.html')
     context = {
